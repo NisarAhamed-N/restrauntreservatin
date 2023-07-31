2789deefd91bea27e194e1e59e1c0fc4d505c681
NisarAhamed-N <119098287+NisarAhamed-N@users.noreply.github.com>
2023-07-31 10:30:13 +0530

    Backend

---
+++ Backend/src/main/java/com/food/Service/UserSignupService.java
@@ -0,0 +1,20 @@
+package com.food.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.food.Entity.UserSignup;
+import com.food.Repository.UserSignupRepository;
+
+@Service
+public class UserSignupService {
+
+	
+		@Autowired 
+		UserSignupRepository repo;
+		public Iterable<UserSignup> GetAll(){
+			return repo.findAll();
+		}
+		
+	}
+
+	
+
